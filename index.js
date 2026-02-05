36e6253f71311b15b6289e8f24670756147163ce
KritikaSingh1775 <kritikasingh1775>
2026-02-05 17:33:36 +0530

    files added

---
+++ index.js
@@ -0,0 +1,22 @@
+const connectToMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
+
+(async () => {
+  await connectToMongo();
+
+  const app = express();
+  const port = 5000;
+  app.use(cors());
+  app.use(express.json());
+
+  app.use("/api/auth", require("./routes/auth"));
+  app.use("/api/notes", require("./routes/note"));
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+})();
